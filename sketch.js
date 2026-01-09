2d3d9fa85869c5ae453c8c8a686b5488d4a6dcde
ikeda-Java2307 <2201100202qv@cyber-u.ac.jp>
2026-01-10 01:34:25 +0900

    gps取得

--- sketch.js
+++ sketch.js
@@ -1,6 +1,23 @@
 let cam;
+const route = [
+  { lat: 37.525140, lon: 138.914896 }
+/*
+  { lat: 35.0003, lon: 139.0005 },
+  { lat: 35.0006, lon: 139.0008 },
+  { lat: 35.0010, lon: 139.0012 }*/
+];
+let targetIndex = 0;
+let targetPoint = route[targetIndex];
+let current = {};
+
+navigator.geolocation.watchPosition(pos => {
+  current.lat = pos.coords.latitude; //current.lat?
+  current.lon = pos.coords.longitude; //current.lon?
+});
+
 
 function setup() {
+	// カメラ機能
   	createCanvas(windowWidth, windowHeight);
   	cam = createCapture({
 		video: {
@@ -13,4 +30,6 @@ function setup() {
 
 function draw() {
     image(cam, 0, 0, width, height);
+	text(current.lat.toString(), 0, 0);
+	text(current.lon.toString(), 0, 10);
 }
