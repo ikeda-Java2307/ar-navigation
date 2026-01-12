5f95dc045b5299902c8d3bdc12770dd69655c223
ikeda-Java2307 <2201100202qv@cyber-u.ac.jp>
2026-01-13 01:27:08 +0900

    修正9

--- sketch.js
+++ sketch.js
@@ -30,7 +30,8 @@ function setup() {
 
 function draw() {
     image(cam, 0, 0, width, height);
-	textSize(100);
-	text(String(current.lat), 0, 0, width, height);
-	text(String(current.lon), 0, 100, width, height);
+	// textSize(50);
+	text(String(current.lat), 0, 0);
+	text(String(current.lon), 0, 50);
+	text("Hello, World", 0, 100);
 }
